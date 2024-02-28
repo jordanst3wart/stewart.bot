c157003b4058754d8424511be69e5d5683d25410
Jordan Stewart <jordanstewart2428@gmail.com>
2024-02-28 17:50:17 +1100

    half blogged

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,17 @@ set -e
 
 yarn build
 
+# copy all the *.html files to just file
+# for file in out/*.html; do cp "$file" out/$(basename "$file"); done
+
 cd out
 
+
+
 # copy to s3 bucket
-aws s3 cp . s3://stewart.bot/ --recursive --profile personal
+aws s3 cp . s3://stewart.bot/ --recursive --profile personal --exclude **/.DS_Store
 
 
 # aws cloudfront create-invalidation --distribution-id E2WLKPSIG60UDN --paths "/*" --profile personal
+
+# TODO add .html to link... :\
